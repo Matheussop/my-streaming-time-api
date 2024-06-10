d23f47c60caa0968c4660856f4331b62b5e6dc41
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2024-06-09 21:00:15 -0300

    feat: Create fetchAndSaveExternalMovies and findOrAddMovie methods

--- src/controllers/movieTMDBController.ts
+++ src/controllers/movieTMDBController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
+import Movie from '../models/movieModel';
 
 
 export const getExternalMovies = async (req: Request, res: Response): Promise<void> => {
@@ -24,6 +25,102 @@ export const getExternalMovies = async (req: Request, res: Response): Promise<vo
       }
     });
     res.status(200).json(myResponse.splice(0, 8));
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Método para fazer a requisição à API externa, modificar o objeto e salvar no banco de dados
+export const fetchAndSaveExternalMovies = async (req: Request, res: Response): Promise<void> => {
+  try {
+    // Faça a requisição à API externa
+    const url = 'https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.TMDB_Bearer_Token}`
+      }
+    };
+    const response = await axios.get(url, options);
+
+    // Suponha que a resposta é uma lista de filmes
+    const externalMovies = response.data.results;
+
+    // Buscar todos os títulos dos filmes existentes no banco de dados
+    const existingMovies = await Movie.find({}, 'title').lean();
+    const existingTitles = existingMovies.map(movie => movie.title);
+
+    // Filtrar e modificar os novos filmes
+    const newMovies = externalMovies.filter((externalMovie: any) => !existingTitles.includes(externalMovie.title))
+      .map((externalMovie: any) => ({
+        title: externalMovie.title,
+        release_date: externalMovie.release_date,
+        plot: externalMovie.overview,
+        rating: externalMovie.vote_average,
+        url: `https://image.tmdb.org/t/p/w500${externalMovie.poster_path}`
+      }));
+
+    // Salvar os novos filmes no banco de dados
+    const savedMovies = await Movie.insertMany(newMovies);
+
+    res.status(201).json(savedMovies);
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+// Novo método para verificar se um filme existe e adicioná-lo se não existir
+export const findOrAddMovie = async (req: Request, res: Response): Promise<void> => {
+  const title = req.body.title as string;
+
+  if (!title) {
+    res.status(400).json({ message: 'Title query parameter is required' });
+    return;
+  }
+
+  try {
+    // Verificar se o filme existe no banco de dados
+    const existingMovie = await Movie.findOne({ title }).lean();
+    if (existingMovie) {
+      res.status(200).json(existingMovie);
+      console.log("Ja Existia o filme no banco")
+      return;
+    }
+
+    const encodedQueryParams = encodeURIComponent(title.trim());
+    // Se não estiver no banco de dados, verificar na API externa
+    const url = `https://api.themoviedb.org/3/search/movie?query=${encodedQueryParams}&include_adult=false&language=pt-BR&page=1`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.TMDB_Bearer_Token}`
+      }
+    };
+    const response = await axios.get(url, options);
+
+    if (response.data.results.length > 0) {
+      const moviesToSave = response.data.results.map((externalMovie: any) => (
+        {
+          title: externalMovie.title,
+          release_date: externalMovie.release_date,
+          plot: externalMovie.overview,
+          rating: externalMovie.vote_average,
+          url: `https://image.tmdb.org/t/p/w500${externalMovie.poster_path}`
+      }));
+    
+
+      // Salvar o novo filme no banco de dados
+      const savedMovies = await Movie.insertMany(moviesToSave);
+
+      console.log("Eram filmes novos, pesquisado e baixado no banco de dados")
+      res.status(200).json(savedMovies);
+    } else {
+      res.status(404).json({ message: 'Movie not found' });
+    }
   } catch (err: any) {
     res.status(500).json({ message: err.message });
   }
--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -6,12 +6,14 @@ import {
   updateMovie,
   deleteMovie,
 } from '../controllers/movieController';
-import { getExternalMovies } from '../controllers/movieTMDBController';
+import { findOrAddMovie, fetchAndSaveExternalMovies, getExternalMovies } from '../controllers/movieTMDBController';
 
 const router: Router = Router();
 
 router.get('/', getMovies);
 router.get('/external', getExternalMovies);
+router.post('/external', fetchAndSaveExternalMovies);
+router.get('/findOrAddMovie', findOrAddMovie);
 router.get('/:id', getMovieById);
 router.post('/', createMovie);
 router.put('/:id', updateMovie);
